88d1ecd4b1caeebb53203a4698ada307a3c76d79
wleite <wleite@student.42sp.org.br>
2021-07-27 18:58:56 -0300

    feat: add ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	size;
+
+	if (!s1 || !s2)
+		return (NULL);
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(sizeof(char) * size);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, size);
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -43,4 +43,6 @@ char	*ft_strdup(const char *s);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
+char	*ft_strjoin(char const *s1, char const *s2);
+
 #endif
