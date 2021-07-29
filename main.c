4fd64d77989482e6ab391c43457d3112b6749d27
wleite <wleite@student.42sp.org.br>
2021-07-29 16:56:08 -0300

    test: ensure ft_putendl_fd is working

--- main.c
+++ main.c
@@ -1338,8 +1338,82 @@ int	main(void)
 	//visual debug
 	// printf("\nFD:%s", putstr_str);
 
+	free(putstr_str);
 	close(putstr_fd_read);
 	close(putstr_fd_write);
+
+	////////////////////////////////
+	//         ft_putendl_fd      //
+	////////////////////////////////
+	char	*putendl_str;
+	char	putendl_str_d[] = { -1, -2, -3, -4, -5, -6, '\0' };
+	int		putendl_fd_read;
+	int		putendl_fd_write;
+
+	putendl_str = (char *)malloc(sizeof(char) * 30);
+
+	//READ WRITE "They floated in the center"
+	memset(putendl_str, 0, 30);
+	putendl_fd_write = open("file.z", O_WRONLY | O_CREAT, 0777);
+	ft_putendl_fd("They floated in the center", putendl_fd_write);
+
+	putendl_fd_read = open("file.z", O_RDONLY);
+	read(putendl_fd_read, putendl_str, strlen("They floated in the center") + 1);
+
+	if (strcmp(putendl_str, "They floated in the center\n") == 0)
+		printf(GRN "FT_PUTENDL_FD:   [OK]" reset);
+	else
+		printf(RED "FT_PUTENDL_FD:   [KO]" reset);
+	//READ WRITE "They floated in the center"
+
+	//visual debug
+	// printf("\nFD:%s", putendl_str);
+
+	//READ WRITE "He tried to walk past her back into the dark"
+	memset(putendl_str, 0, 30);
+	putendl_fd_write = open("file.z", O_WRONLY | O_CREAT, 0777);
+	ft_putendl_fd("He tried to walk past her back into the dark", putendl_fd_write);
+
+	putendl_fd_read = open("file.z", O_RDONLY);
+	read(putendl_fd_read, putendl_str, ft_strlen("He tried to walk past her back into the dark") + 1);
+
+	if (strcmp(putendl_str, "He tried to walk past her back into the dark\n") == 0)
+		printf(GRN " [OK]" reset);
+	else
+		printf(RED " [KO]" reset);
+	//READ WRITE "He tried to walk past her back into the dark"
+
+	//READ WRITE "-1, -2, -3, -4, -5, -6, '\0'"
+	memset(putendl_str, 0, 30);
+	putendl_fd_write = open("file.z", O_WRONLY | O_CREAT, 0777);
+	ft_putendl_fd(putendl_str_d, putendl_fd_write);
+
+	putendl_fd_read = open("file.z", O_RDONLY);
+	read(putendl_fd_read, putendl_str, ft_strlen(putendl_str_d) + 1);
+
+	if (putendl_str[5] == -6 && putendl_str[6] == '\n')
+		printf(GRN " [OK]" reset);
+	else
+		printf(RED " [KO]" reset);
+	//READ WRITE "-1, -2, -3, -4, -5, -6, '\0'"
+
+	//READ WRITE "NULL"
+	putendl_fd_write = open("file.z", O_WRONLY | O_CREAT, 0777);
+	ft_putendl_fd(NULL, putendl_fd_write);
+
+	putendl_fd_read = open("file.z", O_RDONLY);
+	read(putendl_fd_read, putendl_str, ft_strlen(putendl_str_d));
+
+	if (putendl_str[5] == -6 && putendl_str[6] == '\n')
+		printf(GRN " [OK]\n" reset);
+	else
+		printf(RED " [KO]\n" reset);
+	//READ WRITE "NULL"
+
+
+	free(putendl_str);
+	close(putendl_fd_read);
+	close(putendl_fd_write);
 
 	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
 	return (0);
