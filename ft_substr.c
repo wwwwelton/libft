e584b1499712e3bccb76cc9180180d7d585ac3f7
wleite <wleite@student.42sp.org.br>
2021-08-04 05:24:06 -0300

    refactor: rename ft_strnlen in ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:25:30 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:25:31 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 05:23:38 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_strnlen(const char *s, size_t n)
+static size_t	strnlen(const char *s, size_t n)
 {
 	size_t	i;
 
@@ -31,7 +31,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	srclen = ft_strlen(s);
 	if (srclen > start)
-		len = ft_strnlen(&s[start], len);
+		len = strnlen(&s[start], len);
 	else
 		return (ft_strdup(""));
 	str = (char *)malloc(sizeof(char) * len + 1);
