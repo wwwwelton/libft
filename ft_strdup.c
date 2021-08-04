ce31726c3edc0eeb8fcb969341025c9cef2afa91
wleite <wleite@student.42sp.org.br>
2021-08-04 03:37:33 -0300

    refactor: change ft_strdup prototype to match bsd

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,21 +6,21 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:39 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:39 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 01:49:07 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(const char *str)
 {
-	char	*str;
+	char	*s;
 	size_t	len;
 
-	len = ft_strlen(s) + 1;
-	str = (char *)malloc(sizeof(char) * len);
-	if (str == NULL)
+	len = ft_strlen(str) + 1;
+	s = (char *)malloc(sizeof(char) * len);
+	if (s == NULL)
 		return (NULL);
-	ft_strlcpy(str, s, len);
-	return (str);
+	ft_strlcpy(s, str, len);
+	return (s);
 }
