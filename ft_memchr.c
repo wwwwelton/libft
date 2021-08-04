6279e16cad711909fad6b742f4a0f33438192289
wleite <wleite@student.42sp.org.br>
2021-08-04 03:36:37 -0300

    refactor: change ft_memchr prototype to match bsd

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,18 +6,18 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:14 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:14 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 02:29:09 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *b, int c, size_t len)
 {
 	unsigned char	*ptr;
 
-	ptr = (unsigned char *)s;
-	while (n--)
+	ptr = (unsigned char *)b;
+	while (len--)
 	{
 		if (*ptr == (unsigned char)c)
 			return ((void *)ptr);
