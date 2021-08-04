db2c5f64f8bab7cd9f7a1aa5569900b4d96c7b0e
wleite <wleite@student.42sp.org.br>
2021-08-04 04:52:31 -0300

    refactor: move vars declarations in ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:24 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/04 03:56:42 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 04:44:51 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t len)
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
 
-	ptr_dest = (unsigned char *)dst;
-	ptr_src = (unsigned char *)src;
 	if ((!dst && !src) || !len)
 		return (dst);
+	ptr_dest = (unsigned char *)dst;
+	ptr_src = (unsigned char *)src;
 	while (len--)
 		*ptr_dest++ = *ptr_src++;
 	return (dst);
