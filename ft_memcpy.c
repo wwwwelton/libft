f4d4c5c09383890f941d20339f9b7f6fcc92d8c9
wleite <wleite@student.42sp.org.br>
2021-08-04 03:36:59 -0300

    refactor: change ft_memcpy prototype to match bsd

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:24 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:24 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 02:33:50 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
 	size_t			i;
 
-	ptr_dest = (unsigned char *)dest;
+	ptr_dest = (unsigned char *)dst;
 	ptr_src = (unsigned char *)src;
-	if ((!dest && !src) || !n)
-		return (dest);
+	if ((!dst && !src) || !len)
+		return (dst);
 	i = -1;
-	while (++i < n)
+	while (++i < len)
 		ptr_dest[i] = ptr_src[i];
-	return (dest);
+	return (dst);
 }
