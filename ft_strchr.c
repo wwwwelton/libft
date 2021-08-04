14c6a11f5e70442800558b23f7b48a8b266b7e93
wleite <wleite@student.42sp.org.br>
2021-08-04 05:05:59 -0300

    refactor: add new var ch to ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:35 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:36 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 04:59:58 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 char	*ft_strchr(const char *s, int c)
 {
 	char	*str;
+	char	ch;
 
 	str = (char *)s;
+	ch = (char)c;
 	while (*str)
 	{
-		if (*str == (char)c)
+		if (*str == ch)
 			return (str);
 		str++;
 	}
-	if (*str == (char)c)
+	if (*str == ch)
 		return (str);
 	return (NULL);
 }
