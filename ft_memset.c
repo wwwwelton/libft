2b2a5b5c5fbead12caf348a48ba77ef65c42a2b4
wleite <wleite@student.42sp.org.br>
2021-08-04 03:37:24 -0300

    refactor: change ft_memset prototype to match bsd

--- ft_memset.c
+++ ft_memset.c
@@ -6,20 +6,20 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:32 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:32 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 02:44:28 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *dest, int c, size_t len)
 {
 	unsigned char	*ptr;
 	size_t			i;
 
-	ptr = (unsigned char *)s;
+	ptr = (unsigned char *)dest;
 	i = -1;
-	while (++i < n)
+	while (++i < len)
 		ptr[i] = (unsigned char)c;
-	return (s);
+	return (dest);
 }
