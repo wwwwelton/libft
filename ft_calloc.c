fec1d40297fccb0ef458afea0950300b42994595
wleite <wleite@student.42sp.org.br>
2021-08-04 03:36:20 -0300

    refactor: change ft_calloc prototype to match bsd

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:23:23 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:23:25 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 02:23:42 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t number, size_t size)
 {
 	void	*ptr;
 
-	ptr = (void *)malloc(nmemb * size);
+	ptr = (void *)malloc(number * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, nmemb * size);
+	ft_bzero(ptr, number * size);
 	return (ptr);
 }
