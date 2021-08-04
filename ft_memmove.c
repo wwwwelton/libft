e921b75c3e4a1b50f7ce7dad401eaf916ae6e442
wleite <wleite@student.42sp.org.br>
2021-08-04 03:37:14 -0300

    refactor: change ft_memmove prototype to match bsd

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,33 +6,33 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:28 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:28 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 02:40:57 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
 	size_t			i;
 
-	ptr_dest = (unsigned char *)dest;
+	ptr_dest = (unsigned char *)dst;
 	ptr_src = (unsigned char *)src;
-	if ((!dest && !src) || !n)
-		return (dest);
-	if ((char *)dest <= (char *)src)
+	if ((!dst && !src) || !len)
+		return (dst);
+	if ((char *)dst < (char *)src)
 	{
 		i = -1;
-		while (++i < n)
+		while (++i < len)
 			ptr_dest[i] = ptr_src[i];
 	}
 	else
 	{
-		i = n;
+		i = len;
 		while (i--)
 			ptr_dest[i] = ptr_src[i];
 	}
-	return (dest);
+	return (dst);
 }
