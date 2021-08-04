afb0d93860ae7560247f05c02b9dd86493d32063
wleite <wleite@student.42sp.org.br>
2021-08-04 05:29:25 -0300

    refactor: move var declarations in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:55 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:56 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 05:28:16 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t len)
 	unsigned char	*str2;
 	size_t			i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
 	if (len == 0)
 		return (0);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
 	while ((str1[i] == str2[i]) && str1[i] && str2[i] && i < len)
 		i++;
