a817fa298a940dc0b9286469a87d9ac6b36dcf37
wleite <wleite@student.42sp.org.br>
2021-08-04 03:36:51 -0300

    refactor: change ft_memcmp prototype to match bsd

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,20 +6,20 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:18 by wleite            #+#    #+#             */
-/*   Updated: 2021/07/27 19:24:19 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 00:55:28 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *b1, const void *b2, size_t len)
 {
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
 
-	ptr1 = (unsigned char *)s1;
-	ptr2 = (unsigned char *)s2;
-	while (n-- > 0)
+	ptr1 = (unsigned char *)b1;
+	ptr2 = (unsigned char *)b2;
+	while (len-- > 0)
 	{
 		if (*ptr1++ != *ptr2++)
 			return ((*--ptr1) - (*--ptr2));
