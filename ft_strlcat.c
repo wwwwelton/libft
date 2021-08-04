317d3c624cdae282fdb119a866f0caa8e3e8bf9f
wleite <wleite@student.42sp.org.br>
2021-08-04 05:23:10 -0300

    refactor: rename ft_strnlen in ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:45 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/04 05:02:31 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 05:21:50 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_strnlen(const char *s, size_t n)
+static size_t	strnlen(const char *s, size_t n)
 {
 	size_t	i;
 
@@ -28,7 +28,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dstlen;
 
 	srclen = ft_strlen(src);
-	dstlen = ft_strnlen(dst, dstsize);
+	dstlen = strnlen(dst, dstsize);
 	if (dstlen == dstsize)
 		return (dstlen + srclen);
 	ft_strlcpy(&dst[dstlen], src, dstsize - dstlen);
