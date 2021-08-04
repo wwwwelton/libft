1bd64541e49fd1a63e0fa4a3ae15f194c4bfd211
wleite <wleite@student.42sp.org.br>
2021-08-04 04:03:19 -0300

    refactor: removes i from ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:24:24 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/04 02:33:50 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/04 03:56:42 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
-	size_t			i;
 
 	ptr_dest = (unsigned char *)dst;
 	ptr_src = (unsigned char *)src;
 	if ((!dst && !src) || !len)
 		return (dst);
-	i = -1;
-	while (++i < len)
-		ptr_dest[i] = ptr_src[i];
+	while (len--)
+		*ptr_dest++ = *ptr_src++;
 	return (dst);
 }
