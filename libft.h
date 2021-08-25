beff4506b95ce929c9f44c70caae6c40a47fc0bd
wleite <wleite@student.42sp.org.br>
2021-08-25 17:45:45 -0300

    feat: add ft_char_tostr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:25:46 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/25 17:33:00 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/25 17:45:14 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -529,6 +529,16 @@ char	*ft_str_replace(const char *str, char *old_word, char *new_word);
  * @return Returns the number of characters changed. Returns -1
  * if the string is NULL.
  */
-int	ft_str_toupper(char *str);
+int		ft_str_toupper(char *str);
+
+/**
+ * @brief Converts a unsigned char to a string allocated with malloc.
+ *
+ * @param c This is the unsigned char to be converted.
+ * @return Returns a string, allocated with malloc with the character
+ * passed to the parameter in the first position and '\\0' in the
+ * second position.
+ */
+char	*ft_char_tostr(unsigned char c);
 
 #endif
